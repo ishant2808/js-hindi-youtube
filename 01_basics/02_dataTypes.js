74b8f275f6a7a61f24e0f41aeb63c246bcdb649c
ishant2808 <115950193+ishant2808@users.noreply.github.com>
2024-05-06 20:49:09 +0000

    2 day

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+ // alert( 3 + 3 ) // it give error when werun so we are using node.js in IDLE ,not browser
+
+console.log( 3 
+
+    + 3) //code readability be high 
+
+
+console.log("Ishant")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIN = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+//symbol => unique
+
+
+// object 
+
+
+console.log(typeof age);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33abc" 
+//" 33" give string, 33 give number
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" => 33 convert
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIN = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
