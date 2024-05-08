ff5c883e08e910e395406722bde6077e80eafb6c
ishant2808 <115950193+ishant2808@users.noreply.github.com>
2024-05-08 20:21:16 +0000

    conversion and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+// ********************  OPeration  ************************ \\
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);  its give -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); square
+// console.log(2/2); divide
+// console.log(2%2); remainder
+ 
+let str1 ="hello"
+let str2 = "ISahant"
+str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log( 1 + "2" );
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log( (3+4) * 5 % 3);
+
+console.log(true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 =2+2
+
+
+// increment
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
