4e9ccc99dad0a813bfe6384a1a3275b457e8e41c
ishant2808 <115950193+ishant2808@users.noreply.github.com>
2024-05-09 20:35:34 +0000

    all about comparison

--- 01_basics/04_Comparison.js
+++ 01_basics/04_Comparison.js
@@ -1,1 +1,24 @@
-console.log("ishant welcome")
+// console.log(2>1);
+// console.log(2=>1);
+// console.log(2==1);
+// console.log(2<1);
+// console.log(2!=1);
+
+//--------------datatype
+console.log("2">1);
+console.log("02">1);
+
+
+//null=>null/nan change
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+
+// ===
+//in this datatype check in triple equal
+console.log("2" === 2);
